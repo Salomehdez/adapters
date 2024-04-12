ef8e543b464d1130fda40abfc5ff960befbae786
Salomehdez <laurasalomeh40@gmail.com>
2024-04-11 22:40:06 -0500

    Initial commit

---
+++ app/src/main/java/unipiloto/edu/co/DrinkCategoryActivity.java
@@ -0,0 +1,34 @@
+package unipiloto.edu.co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> adapatadorLista = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.bebidas);
+        ListView listaBebidas = (ListView) findViewById(R.id.list_drinks);
+        listaBebidas.setAdapter(adapatadorLista);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        };
+
+        listaBebidas.setOnItemClickListener(itemClickListener);
+    }
+}
